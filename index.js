fede87715907398f2c32e9ad24564782c50a92fd
shonurm77 <shonurm777@gmail.com>
2024-02-27 07:43:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let X, Y, Z, res
+
+X = +prompt('Ведите первое число')
+Y = +prompt('Ведите второе число')
+Z = +prompt('Ведите третье число');
+
+res = (X + Y + Z) / 3
+
+console.log('ваш ответ ' + res);
+alert(res)
